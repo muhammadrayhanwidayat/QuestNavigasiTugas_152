d072b5212b652b8ace5e6510ee492257dc21a816
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-08 19:00:37 +0700

    membuat composable

--- app/src/main/java/com/example/questnavigasitugas_152/view/Welcome.kt
+++ app/src/main/java/com/example/questnavigasitugas_152/view/Welcome.kt
@@ -1,2 +1,17 @@
 package com.example.questnavigasitugas_152.view
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirScreen(navController: NavController) {
+    var nama by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
+
+    var expandedJK by remember { mutableStateOf(false) }
+    var expandedStatus by remember { mutableStateOf(false) }
+
+    val jenisKelaminList = listOf("Laki-laki", "Perempuan")
+    val statusList = listOf("Belum Menikah", "Menikah")
+
